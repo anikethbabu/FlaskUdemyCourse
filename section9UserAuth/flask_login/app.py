ad9398234396743ea5dce80bd887beb6352e87d7
anikethbabu@gmail.com <anikethbabu@gmail.com>
2021-05-14 17:27:27 -0400

    Created login form

--- section9UserAuth/flask_login/app.py
+++ section9UserAuth/flask_login/app.py
@@ -33,7 +33,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
 
-        if user.check_password(form.password.data) and user is not none:
+        if user.check_password(form.password.data) and user is not None:
 
             login_user(user)
             flash("Logged in Successfully!")
@@ -45,12 +45,25 @@ def login():
 
             return redirect(next)
 
-        return redirect("login.html", form=form)
+    return render_template("login.html", form=form)
 
-@app.route("/register",methods=["GET", "POST"])
-def register:
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
     form = RegistrationForm()
 
     if form.validate_on_submit():
-        user = User(email=form.email.data,username=form.username.data,password=form.password.data)
+        user = User(
+            email=form.email.data,
+            username=form.username.data,
+            password=form.password.data,
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash("Thanks for registration!")
+        return redirect("login")
+    return render_template("register.html", form=form)
 
+
+if __name__ == "__main__":
+    app.run(debug=True)
